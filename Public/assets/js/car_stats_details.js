bf363e0b040949a7ff24d4b5252d9dee54128665
ringotom1980 <ringo_tom2007@yahoo.com.tw>
2026-01-17 23:20:29 +0800

    修改右側表格

--- Public/assets/js/car_stats_details.js
+++ Public/assets/js/car_stats_details.js
@@ -3,40 +3,46 @@
  */
 
 (function (global) {
-  'use strict';
+    'use strict';
 
-  function esc(s) {
-    s = (s === null || s === undefined) ? '' : String(s);
-    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;').replace(/'/g, '&#039;');
-  }
-
-  var Mod = {
-    render: function (tbody, rows, fmtInt) {
-      if (!tbody) return;
-      rows = rows || [];
-
-      if (!rows.length) {
-        tbody.innerHTML = '<tr><td colspan="6" class="cs-empty">無明細</td></tr>';
-        return;
-      }
-
-      var html = '';
-      rows.forEach(function (r) {
-        html += '<tr>';
-        html += '<td>' + esc(r.vehicle_code || '') + '</td>';
-        html += '<td>' + esc(r.repair_date || '') + '</td>';
-        html += '<td>' + esc(r.content || '') + '</td>';
-        html += '<td class="ta-r">' + esc(fmtInt(r.company_amount_total || 0)) + '</td>';
-        html += '<td class="ta-r">' + esc(fmtInt(r.team_amount_total || 0)) + '</td>';
-        html += '<td class="ta-r">' + esc(fmtInt(r.grand_total || 0)) + '</td>';
-        html += '</tr>';
-      });
-
-      tbody.innerHTML = html;
+    function esc(s) {
+        s = (s === null || s === undefined) ? '' : String(s);
+        return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;').replace(/'/g, '&#039;');
     }
-  };
 
-  global.CarStatsDetails = Mod;
+    function fmtMoney(v) {
+        var n = Number(v || 0);
+        if (!isFinite(n)) n = 0;
+        return String(Math.round(n)).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    }
+
+    var Mod = {
+        render: function (tbody, rows, fmtInt) {
+            if (!tbody) return;
+            rows = rows || [];
+
+            if (!rows.length) {
+                tbody.innerHTML = '<tr><td colspan="6" class="cs-empty">無明細</td></tr>';
+                return;
+            }
+
+            var html = '';
+            rows.forEach(function (r) {
+                html += '<tr>';
+                html += '<td>' + esc(r.vehicle_code || '') + '</td>';
+                html += '<td>' + esc(r.repair_date || '') + '</td>';
+                html += '<td>' + esc(r.content || '') + '</td>';
+                html += '<td class="ta-r">' + esc(fmtMoney(r.company_amount_total || 0)) + '</td>';
+                html += '<td class="ta-r">' + esc(fmtMoney(r.team_amount_total || 0)) + '</td>';
+                html += '<td class="ta-r">' + esc(fmtMoney(r.grand_total || 0)) + '</td>';
+                html += '</tr>';
+            });
+
+            tbody.innerHTML = html;
+        }
+    };
+
+    global.CarStatsDetails = Mod;
 
 })(window);
